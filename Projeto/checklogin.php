7cd2a0fcc366bf4d90b04605328b6c18f9ab946e
Eduardo Reis <up201303832@gcloud.fe.up.pt>
2015-11-25 10:57:41 +0000

    Adicionado login php

---
+++ Projeto/checklogin.php
@@ -0,0 +1,25 @@
+<?php
+
+$db = new PDO('sqlite:proj.db');
+
+// username and password sent from form
+$myusername=$_POST['myusername'];
+$mypassword=$_POST['mypassword'];
+
+// To protect MySQL injection (more detail about MySQL injection)
+
+$sql="SELECT * FROM User WHERE name='$myusername' and password='$mypassword'";
+$result=$db->query($sql);
+
+// Mysql_num_row is counting table row
+// If result matched $myusername and $mypassword, table row must be 1 row
+if($result->fetch(PDO::FETCH_NUM > 0)){
+
+// Register $myusername, $mypassword and redirect to file "login_success.php"
+session_start();
+header("location:login_success.php");
+}
+else {
+echo 'Wrong Username or Password';
+}
+?>
