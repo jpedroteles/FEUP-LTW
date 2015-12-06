f0e673a7b3e594af644f15586c4a3f8fd272314d
zombobilium <joaocarduarte@gmail.com>
2015-12-06 21:33:24 +0000

    homecss.css adicionado e callcss e home.php alterado

--- Projeto/home.php
+++ Projeto/home.php
@@ -1,25 +1,26 @@
 <?php
 	$db = new PDO('sqlite:sql.db');
 
-  	$stmt = $db->prepare('SELECT * FROM Event');
+  	$stmt = $db->prepare('SELECT * FROM Event WHERE ID = (SELECT MAX(ID)  FROM Event);');
   	$stmt->execute();
   	$result = $stmt->fetchAll();
 ?>
 
 <html>
 	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="homecss.css">
 	</head>
 	<body>
 
-		<div id = events>
-			Últimos eventos
+		<div id ="events">
+			<h2>Últimos eventos</h2>
 			<?  foreach( $result as $row) {?>
-      			<div>
-							<a  href=# onclick="javascript:registerinevent(<?php echo $row['id'] ?>);">
-        			<h3><?=$row['name']?></h3>
-							</a>
-        			<p><?=$row['startDate']?> .. <?=$row['startTime']?></p>
-        			<p><?=$row['local']?></p>
+      			<div class="ultimoseventos">
+					<a  href=# onclick="javascript:registerinevent(<?php echo $row['id'] ?>);">
+        				<h3><?=$row['name']?></h3>
+					</a>
+        			<p><?=$row['startDate']?></p>
       			</div>
 			<? } ?>
 		</div>
